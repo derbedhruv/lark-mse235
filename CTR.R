f111a548007d9f3073adf2b0f3e2c83a44ba178c
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-05-25 21:39:17 -0700

    Add CTR file
    This will be used to simulate the CTR and experiments in R

---
+++ CTR.R
@@ -0,0 +1,2 @@
+# Simulation experiments from the data we collected on CTRs of ads
+# reference: http://www.marketingdistillery.com/2014/09/24/bayesian-modeling-of-click-through-rate-for-small-data/
