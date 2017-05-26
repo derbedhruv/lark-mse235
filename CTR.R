3a435d6041603c07f30ab960ddf3258db6a39ee5
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-05-25 22:43:58 -0700

    Add small changes

--- CTR.R
+++ CTR.R
@@ -1,22 +1,26 @@
 # Simulation experiments from the data we collected on CTRs of ads
 # reference: http://www.marketingdistillery.com/2014/09/24/bayesian-modeling-of-click-through-rate-for-small-data/
 ### BAYESIAN SIMULATION
-beta <- function(mu, v) {
+ad_stats <- function(mu, v) {
   # return beta distribution for an ad
   # mu is the mean CTR for the ad
   # v is the sample size of the ad
   alpha <- mu*v
   beta <- (1-mu)*v
   
-  # pritn variance in CTR
+  # print variance in CTR
   cat('variance in CTR = ', variance_in_ctr <- mu*(1-mu)/v)
   
-  # plot it
-  s <- seq(0, 5*mu, 0.1*mu)
+  # print the upper and lower bound of CTR
+  
+  
+  # plot it - only the section that 'makes sense'
+  # using min(5 times the CTR or 1) - i.e. if the CTR is high enough, just plot in range [0,1]
+  s <- seq(0, min(5*mu,1), min(0.1*mu, 0.01))
   plot(100*s, dbeta(s, alpha, beta), type="l", ylab="Density", xlab = "CTR (%)")
 }
 
 # plot the CTR distribution of an ad
-beta(0.0037, 2155)
+ad_stats(0.0037, 2155)
 
 
