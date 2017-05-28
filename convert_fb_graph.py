5ff27ca114c2dbdeb67727d2886aa5381a5d6f7a
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-05-28 13:54:19 -0700

    Read in facebook_combined
    Same as q5 from hw2 for now

--- convert_fb_graph.py
+++ convert_fb_graph.py
@@ -1,2 +1,11 @@
 # convert facebook graph to format required by the viral marketing simulator
+# we will select edges of node 1912
+import networkx as nx
+
+# read im the file
+g = nx.read_edgelist('facebook_combined.txt')
+
+# get the degree of each node
+# this gives a dictionary mapping each node ID to its degree
+degrees = nx.degree(g)
 
