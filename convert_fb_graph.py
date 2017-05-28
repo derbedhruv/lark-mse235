e96b5400483ac590aa1bbef690af988aa588ba1f
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-05-28 14:32:09 -0700

    Add file writing

--- convert_fb_graph.py
+++ convert_fb_graph.py
@@ -1,5 +1,5 @@
 # convert facebook graph to format required by the viral marketing simulator
-# we will select edges of node 1912
+# we will select all edges in the (small) ego network
 import networkx as nx
 
 # read im the file
@@ -9,3 +9,19 @@ g = nx.read_edgelist('facebook_combined.txt')
 # this gives a dictionary mapping each node ID to its degree
 degrees = nx.degree(g)
 
+# TRANSFORMATION
+# for all edges, convert into directed edge, going from higher degree node to lower degree,
+# and weighted by the degree of the higher one. This will be written out to a file.
+with open('fb_modified.txt', 'w+') as f:
+	for e in g.edges():
+		# convert edge to (degree, edge_id) format
+		edge = map(lambda x: (degrees[x], x), e)
+		if degrees[e[0]] > degrees[e[1]]:
+			# calculate weight
+			weight = max(edge)[0]
+
+			# write nodes to file
+			s = e[0] + ' ' + e[1] + ' \n'
+			f.write(s)
+
+print "Successfully written to fb_modified.txt!"
